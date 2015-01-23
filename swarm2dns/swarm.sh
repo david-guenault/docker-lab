e15fd494a2d692b1a7b051ec03204a533187cafc
David GUENAULT <david.guenault@gmail.com>
2015-01-23 16:41:22 +0100

    Removed unused files

--- swarm2dns/swarm.sh
+++
@@ -1,3 +0,0 @@
-basepath=$(readlink -f $(dirname $0))
-docker rm swarm
-docker run -ti --rm --name swarm -v $basepath/cluster.conf:/cluster.conf -p 2376:2376 dguenault/swarm:latest manage --discovery file:///cluster.conf -H 0.0.0.0:2376
